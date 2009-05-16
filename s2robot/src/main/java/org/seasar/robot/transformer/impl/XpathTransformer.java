6d99593b286b3a361116e263b6ff82851fb49774
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2009-05-16 22:14:33 +0000

    removed invalid xml char.
    git-svn-id: https://www.seasar.org/svn/sandbox/s2robot/trunk@26 a24fa471-b002-4554-a963-9a0eab8552e2

--- s2robot/src/main/java/org/seasar/robot/transformer/impl/XpathTransformer.java
+++ s2robot/src/main/java/org/seasar/robot/transformer/impl/XpathTransformer.java
@@ -23,6 +23,7 @@ import java.util.regex.Pattern;
 import javax.xml.transform.TransformerException;
 
 import org.cyberneko.html.parsers.DOMParser;
+import org.seasar.framework.util.StringUtil;
 import org.seasar.robot.RobotSystemException;
 import org.seasar.robot.entity.ResponseData;
 import org.seasar.robot.entity.ResultData;
@@ -103,13 +104,32 @@ public class XpathTransformer extends HtmlTransformer {
 
     protected String escapeXml(String value) {
         //        return StringEscapeUtils.escapeXml(value);
-        return value//
+        return stripInvalidXMLCharacters(//
+        value//
                 .replaceAll("&", "&amp;")//
                 .replaceAll("<", "&lt;")//
                 .replaceAll(">", "&gt;")//
                 .replaceAll("\"", "&quot;")//
                 .replaceAll("\'", "&apos;")//
-        ;
+        );
+    }
+
+    private String stripInvalidXMLCharacters(String in) {
+        if (StringUtil.isEmpty(in)) {
+            return in;
+        }
+
+        StringBuilder buf = new StringBuilder();
+        char c;
+        for (int i = 0; i < in.length(); i++) {
+            c = in.charAt(i);
+            if ((c == 0x9) || (c == 0xA) || (c == 0xD)
+                    || ((c >= 0x20) && (c <= 0xD7FF))
+                    || ((c >= 0xE000) && (c <= 0xFFFD))
+                    || ((c >= 0x10000) && (c <= 0x10FFFF)))
+                buf.append(c);
+        }
+        return buf.toString();
     }
 
     protected String trimSpace(String value) {

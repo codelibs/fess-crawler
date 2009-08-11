5ecdff8dcf6e331ea834c6226c2aec777858e1d5
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2009-08-11 21:11:47 +0000

    [ROBOT-17] multi-instance support.
    git-svn-id: https://www.seasar.org/svn/sandbox/s2robot/trunk@140 a24fa471-b002-4554-a963-9a0eab8552e2

--- s2robot/src/test/java/org/seasar/robot/S2RobotTest.java
+++ s2robot/src/test/java/org/seasar/robot/S2RobotTest.java
@@ -19,15 +19,21 @@ import java.io.File;
 
 import org.seasar.extension.unit.S2TestCase;
 import org.seasar.framework.util.ResourceUtil;
+import org.seasar.robot.entity.AccessResult;
+import org.seasar.robot.entity.UrlQueue;
 import org.seasar.robot.filter.impl.UrlFilterImpl;
 import org.seasar.robot.service.DataService;
+import org.seasar.robot.service.UrlQueueService;
 import org.seasar.robot.transformer.impl.FileTransformer;
+import org.seasar.robot.util.AccessResultCallback;
 
 public class S2RobotTest extends S2TestCase {
 
     public S2Robot s2Robot;
 
     public DataService dataService;
+
+    public UrlQueueService urlQueueService;
 
     public FileTransformer fileTransformer;
 
@@ -101,5 +107,74 @@ public class S2RobotTest extends S2TestCase {
         s2Robot.awaitTermination();
         assertEquals(maxCount, dataService.getCount(sessionId));
         dataService.delete(sessionId);
+    }
+
+    public void test_execute_2instance() throws Exception {
+        final String url1 = "http://s2robot.sandbox.seasar.org/";
+        final String url2 = "http://s2portlet.seasar.org/";
+        int maxCount = 10;
+        int numOfThread = 10;
+
+        File file = File.createTempFile("s2robot-", "");
+        file.delete();
+        file.mkdirs();
+        file.deleteOnExit();
+        fileTransformer.setPath(file.getAbsolutePath());
+        // TODO use a local server(ex. jetty)
+
+        S2Robot s2Robot1 = (S2Robot) getComponent(S2Robot.class);
+        s2Robot1.setBackground(true);
+        ((UrlFilterImpl) s2Robot1.urlFilter)
+                .setIncludeFilteringPattern("$1$2$3.*");
+        s2Robot1.addUrl(url1);
+        s2Robot1.getRobotContext().setMaxAccessCount(maxCount);
+        s2Robot1.getRobotContext().setNumOfThread(numOfThread);
+
+        S2Robot s2Robot2 = (S2Robot) getComponent(S2Robot.class);
+        s2Robot2.setBackground(true);
+        ((UrlFilterImpl) s2Robot2.urlFilter)
+                .setIncludeFilteringPattern("$1$2$3.*");
+        s2Robot2.addUrl(url2);
+        s2Robot2.getRobotContext().setMaxAccessCount(maxCount);
+        s2Robot2.getRobotContext().setNumOfThread(numOfThread);
+
+        String sessionId1 = s2Robot1.execute();
+        String sessionId2 = s2Robot2.execute();
+
+        assertNotSame(sessionId1, sessionId2);
+        assertNotSame(s2Robot1.robotContext, s2Robot2.robotContext);
+
+        Thread.sleep(1000);
+
+        assertTrue(s2Robot1.robotContext.running);
+        assertTrue(s2Robot2.robotContext.running);
+
+        s2Robot1.awaitTermination();
+        s2Robot2.awaitTermination();
+
+        assertEquals(maxCount, dataService.getCount(sessionId1));
+        assertEquals(maxCount, dataService.getCount(sessionId2));
+
+        UrlQueue urlQueue;
+        while ((urlQueue = urlQueueService.poll(sessionId1)) != null) {
+            assertTrue(urlQueue.getUrl().startsWith(url1));
+        }
+        while ((urlQueue = urlQueueService.poll(sessionId2)) != null) {
+            assertTrue(urlQueue.getUrl().startsWith(url2));
+        }
+
+        dataService.iterate(sessionId1, new AccessResultCallback() {
+            public void iterate(AccessResult accessResult) {
+                assertTrue(accessResult.getUrl().startsWith(url1));
+            }
+        });
+        dataService.iterate(sessionId2, new AccessResultCallback() {
+            public void iterate(AccessResult accessResult) {
+                assertTrue(accessResult.getUrl().startsWith(url2));
+            }
+        });
+
+        dataService.delete(sessionId1);
+        dataService.delete(sessionId2);
     }
 }

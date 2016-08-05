a4447f5e142add041c354a8f99b39726ba2ef573
nullpos <greenstar77@gmail.com>
2016-08-05 18:01:49 +0900

    add ApiExtractor test #33

--- fess-crawler/src/test/java/org/codelibs/fess/crawler/extractor/impl/ApiExtractorTest.java
+++ fess-crawler/src/test/java/org/codelibs/fess/crawler/extractor/impl/ApiExtractorTest.java
@@ -16,20 +16,39 @@
 package org.codelibs.fess.crawler.extractor.impl;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.io.FileUtils;
-import org.codelibs.fess.crawler.entity.ExtractData;
-import org.dbflute.utflute.core.PlainTestCase;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.codelibs.fess.crawler.entity.ExtractData;
+import org.codelibs.fess.crawler.exception.CrawlerSystemException;
+import org.dbflute.utflute.core.PlainTestCase;
+import org.mortbay.jetty.Handler;
+import org.mortbay.jetty.HttpConnection;
+import org.mortbay.jetty.HttpMethods;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.handler.AbstractHandler;
+import org.mortbay.jetty.handler.DefaultHandler;
+import org.mortbay.jetty.handler.HandlerList;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 /**
  * @author shinsuke
  * 
  */
 public class ApiExtractorTest extends PlainTestCase {
+    final int port = 8080;
+
+    final String ATTR_NAME = "filedata";
+
+    private TestApiExtractorServer server;
 
     private ApiExtractor extractor;
 
@@ -37,37 +56,99 @@ public class ApiExtractorTest extends PlainTestCase {
     protected void setUp() throws Exception {
         super.setUp();
 
-        // TODO launch API server for testing
+        server = new TestApiExtractorServer(port);
+        server.start();
 
         extractor = new ApiExtractor();
-        // TODO set parameters
-        extractor.setUrl("http://172.24.231.10:8000/post");
+        extractor.setUrl("http://127.0.0.1:8080/");
         extractor.init();
     }
 
     @Override
     protected void tearDown() throws Exception {
-        // TODO close API server
-
+        server.stop();
         extractor.destroy();
 
         super.tearDown();
     }
 
     public void test_getText() throws Exception {
-        final String content = "./src/test/resources/extractor/image/test.jpg";
+        final String testStr = "testdata";
+        final String content = ATTR_NAME + "," + testStr;
         final Map<String, String> params = new HashMap<String, String>();
-        /*
-        final ExtractData text = extractor.getText(new ByteArrayInputStream(FileUtils.readFileToByteArray(new File(content))), params);
+        //final ExtractData text = extractor.getText(new ByteArrayInputStream(FileUtils.readFileToByteArray(new File(filePath))), params);
+        final ExtractData text = extractor.getText(new ByteArrayInputStream(testStr.getBytes()), params);
         assertEquals(content, text.getContent());
-        for (String key : text.getKeySet()) {
-            for (String s : text.getValues(key)) {
-                System.out.println(key + "," + s);
-            }
-        }
-        System.out.println(text.getContent());
-        */
     }
 
     // TODO other tests
+
+    static class TestApiExtractorServer {
+        private Server server;
+
+        public TestApiExtractorServer(final int port) {
+            server = new Server(port);
+
+            final RequestHandlerImpl request_handler = new RequestHandlerImpl();
+            final HandlerList handlers = new HandlerList();
+            handlers.setHandlers(new Handler[] { request_handler,
+                    new DefaultHandler() });
+            server.setHandler(handlers);
+        }
+
+        public void start() {
+            try {
+                server.start();
+            } catch (final Exception e) {
+                throw new CrawlerSystemException(e);
+            }
+        }
+
+        public void stop() {
+            try {
+                server.stop();
+                server.join();
+            } catch (final Exception e) {
+                throw new CrawlerSystemException(e);
+            }
+        }
+
+        private static class RequestHandlerImpl extends AbstractHandler {
+            public static final String MULTIPART_FORMDATA_TYPE = "multipart/form-data";
+
+            public static boolean isMultipartRequest(HttpServletRequest request) {
+                return request.getContentType() != null
+                        && request.getContentType().startsWith(MULTIPART_FORMDATA_TYPE);
+            }
+
+            @Override
+            public void handle(String target, HttpServletRequest request,
+                    HttpServletResponse response, int dispatch)
+                    throws java.io.IOException, ServletException {
+                
+                if(!isMultipartRequest(request) || !request.getMethod().equals(HttpMethods.POST)) {
+                    response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                    response.getWriter().println(HttpServletResponse.SC_BAD_REQUEST);
+                    HttpConnection.getCurrentConnection().getRequest().setHandled(true);
+                    return;
+                }
+                try {
+                    List<FileItem> multiparts = new ServletFileUpload(new DiskFileItemFactory())
+                            .parseRequest(request);
+                    for (FileItem item : multiparts) {
+                        if (!item.isFormField()) {
+                            // item is not form field.
+                        } else {
+                            String name = item.getFieldName();
+                            String value = item.getString();
+                            response.getWriter().write(name + "," + value);
+                        }
+                    }
+                } catch (FileUploadException e) {
+                    e.printStackTrace();
+                }
+                HttpConnection.getCurrentConnection().getRequest().setHandled(true);
+            }
+        }
+    }
 }

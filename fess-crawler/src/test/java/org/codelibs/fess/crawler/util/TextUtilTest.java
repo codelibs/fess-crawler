dfa85f999859eb5ddf32babbfb8376365a8dea71
Shinsuke Sugaya <shinsuke@apache.org>
2017-06-08 10:08:55 +0900

    update test cases

--- fess-crawler/src/test/java/org/codelibs/fess/crawler/util/TextUtilTest.java
+++ fess-crawler/src/test/java/org/codelibs/fess/crawler/util/TextUtilTest.java
@@ -15,6 +15,12 @@
  */
 package org.codelibs.fess.crawler.util;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
+import org.codelibs.core.lang.StringUtil;
+import org.codelibs.fess.crawler.util.TextUtil.TextNormalizeContext;
 import org.dbflute.utflute.core.PlainTestCase;
 
 /**
@@ -24,99 +30,117 @@ import org.dbflute.utflute.core.PlainTestCase;
 public class TextUtilTest extends PlainTestCase {
 
     public void test_getContent() {
-        assertEquals("", TextUtil.normalizeText((String) null, 100, -1, -1, false));
-        assertEquals("", TextUtil.normalizeText("", 100, -1, -1, false));
-        assertEquals("", TextUtil.normalizeText(" ", 100, -1, -1, false));
-        assertEquals("", TextUtil.normalizeText("  ", 100, -1, -1, false));
-        assertEquals("", TextUtil.normalizeText("\t", 100, -1, -1, false));
-        assertEquals("", TextUtil.normalizeText("\t\t", 100, -1, -1, false));
-        assertEquals("", TextUtil.normalizeText("\t \t", 100, -1, -1, false));
-        assertEquals("aaa bbb", TextUtil.normalizeText("aaa \u00a0 bbb", 100, -1, -1, false));
-        assertEquals("123 abc", TextUtil.normalizeText(" 123 abc ", 100, -1, -1, false));
-        assertEquals("１２３ あいう", TextUtil.normalizeText("　１２３　あいう　", 100, -1, -1, false));
-        assertEquals("123 abc", TextUtil.normalizeText(" 123\nabc ", 100, -1, -1, false));
-        assertEquals("1234567890 1234567890", TextUtil.normalizeText("1234567890 1234567890", 100, -1, -1, false));
+        assertEquals("", normalizeText((String) null, 100, -1, -1, false));
+        assertEquals("", normalizeText("", 100, -1, -1, false));
+        assertEquals("", normalizeText(" ", 100, -1, -1, false));
+        assertEquals("", normalizeText("  ", 100, -1, -1, false));
+        assertEquals("", normalizeText("\t", 100, -1, -1, false));
+        assertEquals("", normalizeText("\t\t", 100, -1, -1, false));
+        assertEquals("", normalizeText("\t \t", 100, -1, -1, false));
+        assertEquals("aaa bbb", normalizeText("aaa \u00a0 bbb", 100, -1, -1, false));
+        assertEquals("123 abc", normalizeText(" 123 abc ", 100, -1, -1, false));
+        assertEquals("１２３ あいう", normalizeText("　１２３　あいう　", 100, -1, -1, false));
+        assertEquals("123 abc", normalizeText(" 123\nabc ", 100, -1, -1, false));
+        assertEquals("1234567890 1234567890", normalizeText("1234567890 1234567890", 100, -1, -1, false));
     }
 
     public void test_getContent_maxAlphanum() {
-        assertEquals("", TextUtil.normalizeText((String) null, 100, 2, -1, false));
-        assertEquals("", TextUtil.normalizeText("", 100, 2, -1, false));
-        assertEquals("", TextUtil.normalizeText(" ", 100, 2, -1, false));
-        assertEquals("", TextUtil.normalizeText("  ", 100, 2, -1, false));
-        assertEquals("", TextUtil.normalizeText("\t", 100, 2, -1, false));
-        assertEquals("", TextUtil.normalizeText("\t\t", 100, 2, -1, false));
-        assertEquals("", TextUtil.normalizeText("\t \t", 100, 2, -1, false));
-        assertEquals("12 ab", TextUtil.normalizeText(" 123 abc ", 100, 2, -1, false));
-        assertEquals("１２３ あいう", TextUtil.normalizeText("　１２３　あいう　", 100, 2, -1, false));
-        assertEquals("12 ab", TextUtil.normalizeText(" 123\nabc ", 100, 2, -1, false));
-        assertEquals("12", TextUtil.normalizeText(" 123abc ", 100, 2, -1, false));
+        assertEquals("", normalizeText((String) null, 100, 2, -1, false));
+        assertEquals("", normalizeText("", 100, 2, -1, false));
+        assertEquals("", normalizeText(" ", 100, 2, -1, false));
+        assertEquals("", normalizeText("  ", 100, 2, -1, false));
+        assertEquals("", normalizeText("\t", 100, 2, -1, false));
+        assertEquals("", normalizeText("\t\t", 100, 2, -1, false));
+        assertEquals("", normalizeText("\t \t", 100, 2, -1, false));
+        assertEquals("12 ab", normalizeText(" 123 abc ", 100, 2, -1, false));
+        assertEquals("１２３ あいう", normalizeText("　１２３　あいう　", 100, 2, -1, false));
+        assertEquals("12 ab", normalizeText(" 123\nabc ", 100, 2, -1, false));
+        assertEquals("12", normalizeText(" 123abc ", 100, 2, -1, false));
     }
 
     public void test_getContent_maxSymbol() {
-        assertEquals("", TextUtil.normalizeText((String) null, 100, -1, 2, false));
-        assertEquals("", TextUtil.normalizeText("", 100, -1, 2, false));
-        assertEquals("", TextUtil.normalizeText(" ", 100, -1, 2, false));
-        assertEquals("", TextUtil.normalizeText("  ", 100, -1, 2, false));
-        assertEquals("", TextUtil.normalizeText("\t", 100, -1, 2, false));
-        assertEquals("", TextUtil.normalizeText("\t\t", 100, -1, 2, false));
-        assertEquals("", TextUtil.normalizeText("\t \t", 100, -1, 2, false));
-        assertEquals("123 abc", TextUtil.normalizeText(" 123 abc ", 100, -1, 2, false));
-        assertEquals("１２３ あいう", TextUtil.normalizeText("　１２３　あいう　", 100, -1, 2, false));
-        assertEquals("123 abc", TextUtil.normalizeText(" 123\nabc ", 100, -1, 2, false));
-        assertEquals("123abc", TextUtil.normalizeText(" 123abc ", 100, -1, 2, false));
+        assertEquals("", normalizeText((String) null, 100, -1, 2, false));
+        assertEquals("", normalizeText("", 100, -1, 2, false));
+        assertEquals("", normalizeText(" ", 100, -1, 2, false));
+        assertEquals("", normalizeText("  ", 100, -1, 2, false));
+        assertEquals("", normalizeText("\t", 100, -1, 2, false));
+        assertEquals("", normalizeText("\t\t", 100, -1, 2, false));
+        assertEquals("", normalizeText("\t \t", 100, -1, 2, false));
+        assertEquals("123 abc", normalizeText(" 123 abc ", 100, -1, 2, false));
+        assertEquals("１２３ あいう", normalizeText("　１２３　あいう　", 100, -1, 2, false));
+        assertEquals("123 abc", normalizeText(" 123\nabc ", 100, -1, 2, false));
+        assertEquals("123abc", normalizeText(" 123abc ", 100, -1, 2, false));
 
-        assertEquals("!!", TextUtil.normalizeText("!!!", 100, -1, 2, false));
-        assertEquals("//", TextUtil.normalizeText("///", 100, -1, 2, false));
-        assertEquals("::", TextUtil.normalizeText(":::", 100, -1, 2, false));
-        assertEquals("@@", TextUtil.normalizeText("@@@", 100, -1, 2, false));
-        assertEquals("[[", TextUtil.normalizeText("[[[", 100, -1, 2, false));
-        assertEquals("``", TextUtil.normalizeText("```", 100, -1, 2, false));
-        assertEquals("{{", TextUtil.normalizeText("{{{", 100, -1, 2, false));
-        assertEquals("~~", TextUtil.normalizeText("~~~", 100, -1, 2, false));
-        assertEquals("!\"", TextUtil.normalizeText("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~", 100, -1, 2, false));
+        assertEquals("!!", normalizeText("!!!", 100, -1, 2, false));
+        assertEquals("//", normalizeText("///", 100, -1, 2, false));
+        assertEquals("::", normalizeText(":::", 100, -1, 2, false));
+        assertEquals("@@", normalizeText("@@@", 100, -1, 2, false));
+        assertEquals("[[", normalizeText("[[[", 100, -1, 2, false));
+        assertEquals("``", normalizeText("```", 100, -1, 2, false));
+        assertEquals("{{", normalizeText("{{{", 100, -1, 2, false));
+        assertEquals("~~", normalizeText("~~~", 100, -1, 2, false));
+        assertEquals("!\"", normalizeText("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~", 100, -1, 2, false));
     }
 
     public void test_getContent_removeDuplication() {
-        assertEquals("", TextUtil.normalizeText((String) null, 100, -1, -1, true));
-        assertEquals("", TextUtil.normalizeText("", 100, -1, -1, true));
-        assertEquals("", TextUtil.normalizeText(" ", 100, -1, -1, true));
-        assertEquals("", TextUtil.normalizeText("  ", 100, -1, -1, true));
-        assertEquals("", TextUtil.normalizeText("\t", 100, -1, -1, true));
-        assertEquals("", TextUtil.normalizeText("\t\t", 100, -1, -1, true));
-        assertEquals("", TextUtil.normalizeText("\t \t", 100, -1, -1, true));
-        assertEquals("aaa bbb", TextUtil.normalizeText("aaa \u00a0 bbb", 100, -1, -1, true));
-        assertEquals("123 abc", TextUtil.normalizeText(" 123 abc ", 100, -1, -1, true));
-        assertEquals("１２３ あいう", TextUtil.normalizeText("　１２３　あいう　", 100, -1, -1, true));
-        assertEquals("123 abc", TextUtil.normalizeText(" 123\nabc ", 100, -1, -1, true));
+        assertEquals("", normalizeText((String) null, 100, -1, -1, true));
+        assertEquals("", normalizeText("", 100, -1, -1, true));
+        assertEquals("", normalizeText(" ", 100, -1, -1, true));
+        assertEquals("", normalizeText("  ", 100, -1, -1, true));
+        assertEquals("", normalizeText("\t", 100, -1, -1, true));
+        assertEquals("", normalizeText("\t\t", 100, -1, -1, true));
+        assertEquals("", normalizeText("\t \t", 100, -1, -1, true));
+        assertEquals("aaa bbb", normalizeText("aaa \u00a0 bbb", 100, -1, -1, true));
+        assertEquals("123 abc", normalizeText(" 123 abc ", 100, -1, -1, true));
+        assertEquals("１２３ あいう", normalizeText("　１２３　あいう　", 100, -1, -1, true));
+        assertEquals("123 abc", normalizeText(" 123\nabc ", 100, -1, -1, true));
 
-        assertEquals("", TextUtil.normalizeText((String) null, 100, 2, -1, true));
-        assertEquals("", TextUtil.normalizeText("", 100, 2, -1, true));
-        assertEquals("", TextUtil.normalizeText(" ", 100, 2, -1, true));
-        assertEquals("", TextUtil.normalizeText("  ", 100, 2, -1, true));
-        assertEquals("", TextUtil.normalizeText("\t", 100, 2, -1, true));
-        assertEquals("", TextUtil.normalizeText("\t\t", 100, 2, -1, true));
-        assertEquals("", TextUtil.normalizeText("\t \t", 100, 2, -1, true));
-        assertEquals("12 ab", TextUtil.normalizeText(" 123 abc ", 100, 2, -1, true));
-        assertEquals("１２３ あいう", TextUtil.normalizeText("　１２３　あいう　", 100, 2, -1, true));
-        assertEquals("12 ab", TextUtil.normalizeText(" 123\nabc ", 100, 2, -1, true));
-        assertEquals("12", TextUtil.normalizeText(" 123abc ", 100, 2, -1, true));
+        assertEquals("", normalizeText((String) null, 100, 2, -1, true));
+        assertEquals("", normalizeText("", 100, 2, -1, true));
+        assertEquals("", normalizeText(" ", 100, 2, -1, true));
+        assertEquals("", normalizeText("  ", 100, 2, -1, true));
+        assertEquals("", normalizeText("\t", 100, 2, -1, true));
+        assertEquals("", normalizeText("\t\t", 100, 2, -1, true));
+        assertEquals("", normalizeText("\t \t", 100, 2, -1, true));
+        assertEquals("12 ab", normalizeText(" 123 abc ", 100, 2, -1, true));
+        assertEquals("１２３ あいう", normalizeText("　１２３　あいう　", 100, 2, -1, true));
+        assertEquals("12 ab", normalizeText(" 123\nabc ", 100, 2, -1, true));
+        assertEquals("12", normalizeText(" 123abc ", 100, 2, -1, true));
 
-        assertEquals("!!", TextUtil.normalizeText("!!!", 100, -1, 2, true));
-        assertEquals("//", TextUtil.normalizeText("///", 100, -1, 2, true));
-        assertEquals("::", TextUtil.normalizeText(":::", 100, -1, 2, true));
-        assertEquals("@@", TextUtil.normalizeText("@@@", 100, -1, 2, true));
-        assertEquals("[[", TextUtil.normalizeText("[[[", 100, -1, 2, true));
-        assertEquals("``", TextUtil.normalizeText("```", 100, -1, 2, true));
-        assertEquals("{{", TextUtil.normalizeText("{{{", 100, -1, 2, true));
-        assertEquals("~~", TextUtil.normalizeText("~~~", 100, -1, 2, true));
-        assertEquals("!\"", TextUtil.normalizeText("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~", 100, -1, 2, true));
+        assertEquals("!!", normalizeText("!!!", 100, -1, 2, true));
+        assertEquals("//", normalizeText("///", 100, -1, 2, true));
+        assertEquals("::", normalizeText(":::", 100, -1, 2, true));
+        assertEquals("@@", normalizeText("@@@", 100, -1, 2, true));
+        assertEquals("[[", normalizeText("[[[", 100, -1, 2, true));
+        assertEquals("``", normalizeText("```", 100, -1, 2, true));
+        assertEquals("{{", normalizeText("{{{", 100, -1, 2, true));
+        assertEquals("~~", normalizeText("~~~", 100, -1, 2, true));
+        assertEquals("!\"", normalizeText("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~", 100, -1, 2, true));
 
-        assertEquals("aaa bbb ccc", TextUtil.normalizeText("aaa bbb aaa ccc aaa", 100, -1, -1, true));
-        assertEquals("aaa? bbb ccc", TextUtil.normalizeText("aaa? bbb aaa? ccc", 100, -1, -1, true));
-        assertEquals("aaa #bbb ccc?", TextUtil.normalizeText("aaa #bbb# aaa ccc? aaa", 100, -1, -1, true));
-        assertEquals("123 abc", TextUtil.normalizeText(" 123 abc 123", 100, -1, -1, true));
-        assertEquals("あいう １２３ あいう", TextUtil.normalizeText("　あいう　１２３　あいう　", 100, -1, -1, true));
-        assertEquals("123 abc", TextUtil.normalizeText(" 123\nabc\n123 ", 100, -1, -1, true));
-        assertEquals("123# !$", TextUtil.normalizeText(" 123#123!$123  ", 100, -1, -1, true));
+        assertEquals("aaa bbb ccc", normalizeText("aaa bbb aaa ccc aaa", 100, -1, -1, true));
+        assertEquals("aaa? bbb ccc", normalizeText("aaa? bbb aaa? ccc", 100, -1, -1, true));
+        assertEquals("aaa #bbb ccc?", normalizeText("aaa #bbb# aaa ccc? aaa", 100, -1, -1, true));
+        assertEquals("123 abc", normalizeText(" 123 abc 123", 100, -1, -1, true));
+        assertEquals("あいう １２３ あいう", normalizeText("　あいう　１２３　あいう　", 100, -1, -1, true));
+        assertEquals("123 abc", normalizeText(" 123\nabc\n123 ", 100, -1, -1, true));
+        assertEquals("123# !$", normalizeText(" 123#123!$123  ", 100, -1, -1, true));
+    }
+
+    public static String normalizeText(final String str, final int initialCapacity, final int maxAlphanumTermSize,
+            final int maxSymbolTermSize, final boolean removeDuplication) {
+        if (str == null) {
+            return StringUtil.EMPTY;
+        }
+        try (final Reader reader = new StringReader(str)) {
+            return normalizeText(reader, initialCapacity, maxAlphanumTermSize, maxSymbolTermSize, removeDuplication);
+        } catch (final IOException e) {
+            return StringUtil.EMPTY;
+        }
+    }
+
+    public static String normalizeText(final Reader reader, final int initialCapacity, final int maxAlphanumTermSize,
+            final int maxSymbolTermSize, final boolean removeDuplication) {
+        return new TextNormalizeContext(reader).initialCapacity(initialCapacity).maxAlphanumTermSize(maxAlphanumTermSize)
+                .maxSymbolTermSize(maxSymbolTermSize).duplicateTermRemoved(removeDuplication).execute();
     }
 }

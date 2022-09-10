cf93ce49a8fbbf418928f3aac88ecd2226522ca9
Shinsuke Sugaya <shinsuke@apache.org>
2022-09-10 13:49:16 +0900

    fix javadoc

--- fess-crawler/src/main/java/org/codelibs/fess/crawler/util/XPathAPI.java
+++ fess-crawler/src/main/java/org/codelibs/fess/crawler/util/XPathAPI.java
@@ -77,11 +77,9 @@ public class XPathAPI {
 
     /**
      * Use an XPath string to select a single node.
-     * XPath namespace prefixes are resolved from the namespaceNode.
      *
      * @param contextNode The node to start searching from.
      * @param expression A valid XPath string.
-     * @param namespaceNode The node from which prefixes in the XPath will be resolved to namespaces.
      * @return The first node found that matches the XPath, or null.
      *
      * @throws XPathExpressionException
